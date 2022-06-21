f0a8d0c218491ac56706b97869b8add4ae8b0300
Kaike Yudi <kaikeyudi@outlook.com>
2022-06-21 09:32:46 -0300

    req-2: cria endpoint GET /talker/:id

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -11,4 +11,15 @@ router.get('/', async (_req, res) => {
   res.status(200).json(talkers);
 })
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readContentFile(PATH_FILE) || [];
+
+  const talker = talkers.find((talker) => talker.id === Number(id));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada'});
+
+  res.status(200).json(talker);
+});
+
 module.exports = router;
